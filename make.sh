12b7bda859a160cac909f5cf56784169a9eb77ad
ggarabs <ggarabetti0607@gmail.com>
2024-04-25 21:31:51 -0300

    finished

--- make.sh
+++ make.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ! command -v dot &> /dev/null
+then
+    echo "Instalando graphviz..."
+    sudo apt update
+    sudo apt install graphviz
+fi
+
 cd src
 
 javac *.java
@@ -7,3 +14,7 @@ javac *.java
 java Main.java > saida.txt
 
 rm -rf *.class
+
+mv AdjGraph.png ./..
+
+rm AdjGraph.dot
